cd0d766e787391e7469c19ee655ff079ef9bc8af
VERAMEDIC <shadrachrdz@gmail.com>
2026-09-12 14:19:51 -0600

    Update cache version to 'goldent-odontograma-v2-1-r4'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'goldent-odontograma-v2-1-r3';
+const CACHE = 'goldent-odontograma-v2-1-r4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./goldent-logo.png','./goldent-logo.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
